49543ab30436c2ac431ad6a3b1e8e35af1bee20c
Ana Laura <104836958+spadiniAna@users.noreply.github.com>
2022-07-26 17:19:10 -0300

    Update cadastro.php

--- veganoficial/frontend/cadastro.php
+++ veganoficial/frontend/cadastro.php
@@ -12,6 +12,11 @@
     </head>
 
     <body>
+        
+        
+        <?php
+            include_once 'header.php';
+        ?>
 
     <style>
 
@@ -50,44 +55,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
  
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand text-light" href="#">++Vegan</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active text-light" aria-current="page" href="pag_inicial.php">Página inicial</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="receitas.php">Receitas</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="blog.php">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="mapa.php">Mapa</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="saude.php">Saúde</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-              <ul class="navbar-nav justify-content-end me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link text-light" href="login.php">Login</a>
-                </li>
-                <div class="vr text-light"></div>
-                <li class="nav-item">
-                    <a class="nav-link text-light" href="cadastro.php">Cadastre-se</a>
-                </li>
-              </ul>
-          </div>
-        </div>
-      </nav>
+   
     <br>
 
     <div class="container-sm">
