b936fcae5cbc32a1d0c521e1910cc0a8132c813d
Ana Laura <104836958+spadiniAna@users.noreply.github.com>
2022-07-26 17:20:35 -0300

    Update mapa.php

--- veganoficial/frontend/mapa.php
+++ veganoficial/frontend/mapa.php
@@ -13,45 +13,12 @@
     </head>
     <body style="background-color: rgb(255, 244, 213);">
 
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand text-light" href="#">++Vegan</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link text-light" aria-current="page" href="pag_inicial.php">Página inicial</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="receitas.php">Receitas</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="blog.php">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active text-light" href="mapa.php">Mapa</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-light" href="saude.php">Saúde</a>
-              </li>
-            </ul>
-          </div>
-          <div>
-              <ul class="navbar-nav justify-content-end me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link text-light" href="login.php">Login</a>
-                </li>
-                <div class="vr text-light"></div>
-                <li class="nav-item">
-                    <a class="nav-link text-light" href="cadastro.php">Cadastre-se</a>
-                </li>
-              </ul>
-          </div>
-        </div>
-      </nav>
-
+    
+        <?php
+            include_once 'header.php';
+        ?>
+        
+        
     <div class="container">
 
     <h3 class="display-5 text-center" style="color: #92a34f;">Mapa</h3>
